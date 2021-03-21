0217d5b4b81f5653c222433d8b70060c8dda7140
Pavel Shebarshin <pvshebarshin@edu.hse.ru>
2021-03-21 17:42:09 +0300

    reformat ShipsCreatorTest

--- tests/support/ShipsCreatorTest.java
+++ tests/support/ShipsCreatorTest.java
@@ -1,65 +1,64 @@
 package support;
 
 import ocean.Ocean;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ShipsCreatorTest {
 
     @Test
-    void putBattleship() {
+    void putBattleshipTest() {
         Ocean ocean = new Ocean();
         ShipsCreator.putBattleship(ocean);
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                ocean.shootAt(i,j);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                ocean.shootAt(i, j);
             }
         }
-        assert (ocean.getHitCount() == 4);
-        assert (ocean.getShipsSunk() == 1);
-        assert (ocean.isGameOver());
+        Assertions.assertEquals(4, ocean.getHitCount());
+        Assertions.assertEquals(1, ocean.getShipsSunk());
+        Assertions.assertTrue(ocean.isGameOver());
     }
 
     @Test
-    void putCruiser() {
+    void putCruiserTest() {
         Ocean ocean = new Ocean();
         ShipsCreator.putCruiser(ocean);
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                ocean.shootAt(i,j);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                ocean.shootAt(i, j);
             }
         }
-        assert (ocean.getHitCount() == 6);
-        assert (ocean.getShipsSunk() == 2);
-        assert (ocean.isGameOver());
+        Assertions.assertEquals(6, ocean.getHitCount());
+        Assertions.assertEquals(2, ocean.getShipsSunk());
+        Assertions.assertTrue(ocean.isGameOver());
     }
 
     @Test
-    void putDestroyer() {
+    void putDestroyerTest() {
         Ocean ocean = new Ocean();
         ShipsCreator.putDestroyer(ocean);
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                ocean.shootAt(i,j);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                ocean.shootAt(i, j);
             }
         }
-        assert (ocean.getHitCount() == 6);
-        assert (ocean.getShipsSunk() == 3);
-        assert (ocean.isGameOver());
+        Assertions.assertEquals(6, ocean.getHitCount());
+        Assertions.assertEquals(3, ocean.getShipsSunk());
+        Assertions.assertTrue(ocean.isGameOver());
     }
 
     @Test
-    void putSubmarine() {
+    void putSubmarineTest() {
         Ocean ocean = new Ocean();
         ShipsCreator.putSubmarine(ocean);
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                ocean.shootAt(i,j);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                ocean.shootAt(i, j);
             }
         }
-        assert (ocean.getHitCount() == 4);
-        assert (ocean.getShipsSunk() == 4);
-        assert (ocean.isGameOver());
+        Assertions.assertEquals(4, ocean.getHitCount());
+        Assertions.assertEquals(4, ocean.getShipsSunk());
+        Assertions.assertTrue(ocean.isGameOver());
     }
 }
